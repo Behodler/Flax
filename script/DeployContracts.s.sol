5b10033cef0980f05c8afba1a190024be0cab38c
gititgoro <admin@behodler.io>
2024-04-25 06:13:24 +0200

    Mock Token and added CORS

--- script/DeployContracts.s.sol
+++ script/DeployContracts.s.sol
@@ -7,26 +7,58 @@ import "../src/Issuer.sol";
 
 contract DeployContracts is Script {
     function run() public {
-        AddressToString addressToString  = new AddressToString();
+        AddressToString addressToString = new AddressToString();
         vm.startBroadcast();
 
         // Deploy Coupon
-        Coupon coupon = new Coupon("Coupon","CSCX");
+        Coupon coupon = new Coupon("Coupon", "CSCX");
+        Coupon mockInputTokenBurnable = new Coupon("EYE", "EYE");
+        Coupon mockInputTokenNonBurnable = new Coupon(
+            "Uni V2 EYE/SCX",
+            "EYE/SCX"
+        );
 
         // Deploy Issuer with the address of Coupon
         Issuer issuer = new Issuer(address(coupon));
-
+        issuer.setMaxIssuancePerDay(100 ether);
+        issuer.setTokenInfo(address(mockInputTokenBurnable), true, true, 2e12);
+        issuer.setTokenInfo(
+            address(mockInputTokenNonBurnable),
+            true,
+            false,
+            304e10
+        );
         vm.stopBroadcast();
 
-        // Write addresses to a JSON file using Script's file system capabilities
-        string memory jsonOutput = string(abi.encodePacked(
-            '{"Coupon":"', addressToString.toAsciiString(address(coupon)),
-            '", "Issuer":"', addressToString.toAsciiString(address(issuer)), '"}'
-        ));
+        // Creating a JSON array of input token addresses
+        string memory inputs = string(
+            abi.encodePacked(
+                '["',
+                addressToString.toAsciiString(address(mockInputTokenBurnable)),
+                '", "',
+                addressToString.toAsciiString(
+                    address(mockInputTokenNonBurnable)
+                ),
+                '"]'
+            )
+        );
+
+        // Constructing the full JSON output
+        string memory jsonOutput = string(
+            abi.encodePacked(
+                '{"Coupon":"',
+                addressToString.toAsciiString(address(coupon)),
+                '", "Issuer":"',
+                addressToString.toAsciiString(address(issuer)),
+                '", "Inputs":',
+                inputs,
+                "}"
+            )
+        );
         // vm.writeFile("/home/justin/code/BehodlerReborn/coupon/script/output/deployed_addresses.json", jsonOutput);
         console.log(jsonOutput);
     }
-    
+
     function char(bytes1 b) internal pure returns (bytes1 c) {
         if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
         else return bytes1(uint8(b) + 0x57);
